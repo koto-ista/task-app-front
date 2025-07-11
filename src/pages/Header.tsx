8b860d96e86b7cfc01afd8999e19581938ed2db8
koto-ista <kaito.koto@i-standard.jp>
2025-07-11 15:32:17 +0900

    タスク新規登録画面作成

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Button from '../modules/Button';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <header>
       <div className="header-wrapper">
@@ -9,7 +11,7 @@ const Header = () => {
         <h2 className="logo-sub">タスク管理アプリ</h2>
       </div>
       <div className="header-button-wrapper">
-        <Button className="top" label="新規" onClick={() => {}}/>
+        <Button className="top" label="新規" onClick={() => navigate('/add')}/>
       </div>
     </header>
   );
--- src/pages/Tasks/TaskAdd.tsx
+++ src/pages/Tasks/TaskAdd.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import Button from '../../modules/Button';
 
 const TaskAdd = () => {
-  return <div>TaskAdd</div>;
+    return (
+        <div className="task-add-wrapper">
+            <form action="">
+                <div className="task-add-form-wrapper">
+                    <div className="task-add-header">
+                        <input type="text" placeholder="タイトル" />
+                    </div>
+                    <div className="task-add-body">
+                        <textarea name="" id="" placeholder="内容"></textarea>
+                    </div>
+                </div>
+                <div className="task-add-footer">
+                    <Button className="primary" label="登録" onClick={() => { }} />
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default TaskAdd;
