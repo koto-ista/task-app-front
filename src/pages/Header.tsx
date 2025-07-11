de37612fca59d4e2a103ce30c4cb49037e75449f
koto-ista <kaito.koto@i-standard.jp>
2025-07-11 11:26:28 +0900

    Headerの作成

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <div className="header-wrapper">
+        <h1 className="logo">メモメモ</h1>
+        <h2 className="logo-sub">タスク管理アプリ</h2>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
