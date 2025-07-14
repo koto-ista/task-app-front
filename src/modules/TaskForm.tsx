701ce6fee585a938a3d7394de891a35450f75249
koto-ista <kaito.koto@i-standard.jp>
2025-07-14 13:44:21 +0900

    TaskFormを切り分け

--- src/modules/TaskForm.tsx
+++ src/modules/TaskForm.tsx
@@ -1,16 +1,53 @@
 import React, { useState } from 'react';
 import { Task } from '../pages/Base';
+import { useNavigate } from 'react-router-dom';
+import Button from './Button';
 
 interface Props {
-    task: Task;
+    task?: Task;
     setTask: (task: Task) => void;
 }
 
 const TaskForm = (props: Props) => {
-    const [task, setTask] = useState<Task>(props.task);
+    const navigate = useNavigate();
+
+    // 状態変数の定義
+    const [task, setTask] = useState<Task>({
+        id: 0,
+        title: '',
+        content: '',
+        status: 1
+    });
+
+    // フォームの入力値をセットする関数
+    const handleTitleChange = (e: any) => {
+        setTask({ ...task, title: e.target.value });
+    };
+    const handleContentChange = (e: any) => {
+        setTask({ ...task, content: e.target.value });
+    };
+
+    // フォームの送信を処理する関数
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        props.setTask(task);
+        navigate('/');
+    };
     return (
         <div>
-            <h1>TaskForm</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="task-form-wrapper">
+                    <div className="task-header">
+                        <input type="text" placeholder="タイトル" value={task.title} onChange={handleTitleChange} />
+                    </div>
+                    <div className="task-body">
+                        <textarea name="" id="" placeholder="内容" value={task.content} onChange={handleContentChange}></textarea>
+                    </div>
+                </div>
+                <div className="task-footer">
+                    <Button className="primary" label="登録" />
+                </div>
+            </form>
         </div>
     );
 };
--- src/pages/Tasks/TaskAdd.tsx
+++ src/pages/Tasks/TaskAdd.tsx
@@ -2,52 +2,16 @@ import React, { useState } from 'react';
 import Button from '../../modules/Button';
 import { useNavigate } from 'react-router-dom';
 import { Task } from '../Base';
+import TaskForm from '../../modules/TaskForm';
 
 interface Props {
     addTask: (task: Task) => void;
 }
 
 const TaskAdd = (props: Props) => {
-    const navigate = useNavigate();
-
-    // 状態変数の定義
-    const [task, setTask] = useState<Task>({
-        id: 0,
-        title: '',
-        content: '',
-        status: 1
-    });
-
-    // フォームの入力値をセットする関数
-    const handleTitleChange = (e: any) => {
-        setTask({ ...task, title: e.target.value });
-    };
-    const handleContentChange = (e: any) => {
-        setTask({ ...task, content: e.target.value });
-    };
-
-    // フォームの送信を処理する関数
-    const handleSubmit = (e: any) => {
-        e.preventDefault();
-        props.addTask(task);
-        navigate('/');
-    };
-
     return (
         <div className="task-wrapper">
-            <form onSubmit={handleSubmit}>
-                <div className="task-form-wrapper">
-                    <div className="task-header">
-                        <input type="text" placeholder="タイトル" value={task.title} onChange={handleTitleChange} />
-                    </div>
-                    <div className="task-body">
-                        <textarea name="" id="" placeholder="内容" value={task.content} onChange={handleContentChange}></textarea>
-                    </div>
-                </div>
-                <div className="task-footer">
-                    <Button className="primary" label="登録" />
-                </div>
-            </form>
+            <TaskForm setTask={props.addTask} />
         </div>
     );
 };
